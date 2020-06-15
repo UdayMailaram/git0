04b5f1d0710ad051361a220f3de42a506c9e893a
UdayMailaram <66926157+UdayMailaram@users.noreply.github.com>
2020-06-15 15:23:02 +0530

    Update DayOne.js

--- DayOne.js
+++ DayOne.js
@@ -12,7 +12,7 @@ color:"green";
 <h2 id="uday">What Can JavaScript Do?</h2>
 <h3 class="kumar">this is myclass</h3>
 
-<p id="demo">JavaScript can change HTML content.</p>
+<p id="demo">JavaScript can change HTML content</p>
 
 <button type="button" onclick='document.getElementById("uday").innerHTML = "Hello JavaScript!"'>Click Me!</button>
 
