082c4a7032052be6453fb4b143b99f935eb0573f
Chary1729 <mailaramudaykumar98231@gmail.com>
2020-06-15 15:22:14 +0530

    A New Js File

---
+++ DayOne.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.kumar{
+color:"green";
+}
+</style>
+</head>
+<body>
+
+<h2 id="uday">What Can JavaScript Do?</h2>
+<h3 class="kumar">this is myclass</h3>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("uday").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+</body>
+</html>
